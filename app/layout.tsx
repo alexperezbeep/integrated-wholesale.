1e2388b8eac90a17baec9407b1da585c085d760e
alexperezbeep <alexandergperezz@gmail.com>
2025-07-12 19:19:23 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+// app/layout.tsx
+import "./globals.css";
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
